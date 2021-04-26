93bc75339dda59c3b48e50b2d9078ab100708f6d
MaxLanglet <maxime.langlet@ulb.ac.be>
2021-04-26 14:37:03 +0200

    add make.py

---
+++ make.py
@@ -0,0 +1,33 @@
+import subprocess
+import time
+import os
+
+paths = []
+
+for root, dirs, files in os.walk("Instances"):
+    for file in files:
+        if file.endswith(".txt"):
+             #print(os.path.join(root, file))
+             paths.append(os.path.join(file))
+
+paths.sort()
+
+results = open("best_solutions.txt", "w")
+# results = open("VND_tei_2.txt", "w")
+for filename in paths:
+	if filename=='FLP-100-40-1.txt' or filename=='FLP-150-45-1.txt':
+		pass
+	else:
+		start = time.time()
+		print(filename)
+		proc = subprocess.call(["python","flp.py",filename], stdout=results)
+		end = time.time()
+		print(end-start)#add in file
+		if end-start>10*60:
+			print(end-start, "superior to 10 min")
+			break
+# 	end = time.time()
+# 	print(end - start)
+# 	start = time.time()
+# end = time.time()
+# print(end - start)
